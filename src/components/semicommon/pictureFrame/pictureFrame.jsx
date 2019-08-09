1d437071d337ad238e4b1899274b6c6ca9352429
Stefan Djordjevic <ste.djordjevic@gmail.com>
2019-08-09 19:53:37 +0200

    end date start

--- src/components/semicommon/pictureFrame/pictureFrame.jsx
+++ src/components/semicommon/pictureFrame/pictureFrame.jsx
@@ -5,7 +5,6 @@ import styles from "./pictureFrame.module.css";
 export default function PictureFrame(props) {
   return (
     <div className={styles.pictureDiv + " row"}>
-     
       <div className={styles.pictureCol + " col-sm"}>
         <div className={styles.imgFrame}>
           <img className="img-fluid" src={props.imgPath} alt="" />
